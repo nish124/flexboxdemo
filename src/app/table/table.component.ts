857ff505145915ba1b06520ce2a845c1d5a8afac
Nisha_ <ndhere@grepruby.io_>
2020-05-23 04:24:56 +0530

    initial commit

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent{
+
+@Input() value:number;
+
+}
